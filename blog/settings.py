7ddf38bcba5c96c74d1b8dfe0c1935d4d226de55
Nenad Crnoseljanski <45356940+Nenad2497@users.noreply.github.com>
2022-01-24 13:00:51 +0100

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -132,8 +132,8 @@ STATICFILES_DIRS = [
 
 
 
-#STATIC_ROOT = os.path.join(BASE_DIR,'static')
-SMEDIA_ROOT = os.path.join(BASE_DIR,'media')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 
 
